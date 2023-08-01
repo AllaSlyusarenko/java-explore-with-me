63de32817f59b1c502de3aa3b9a1bebf48138ef0
Alla <alla.slyu@yandex.ru>
2023-08-01 22:00:41 +0300

    comments

--- main-service/src/main/java/ru/practicum/ewm/EWMMainServiceApp.java
+++ main-service/src/main/java/ru/practicum/ewm/EWMMainServiceApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EWMMainServiceApp {
     public static void main(String[] args) {
-
         SpringApplication.run(EWMMainServiceApp.class, args);
     }
 }
